42b45cfd7619b531612dff929f37695bdb940ee5
GyeongHWa <hmgyng2017@gmail.com>
2024-01-28 23:18:37 +0900

    시작화면에서 Activity 전환

--- app/src/main/java/com/android/a13app/MainActivity.kt
+++ app/src/main/java/com/android/a13app/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.android.a13app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnLogin: Button //로그인 버튼
+    lateinit var btnRegister: Button //회원가입 버튼
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin = findViewById(R.id.btnLogin)
+        btnRegister = findViewById(R.id.btnRegister)
+
+        btnLogin.setOnClickListener {
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        btnRegister.setOnClickListener {
+            var intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
