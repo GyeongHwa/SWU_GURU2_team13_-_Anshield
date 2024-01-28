cdfdffdfb8a914bfcd897ace2f26700b60ce7672
GyeongHWa <hmgyng2017@gmail.com>
2024-01-29 01:36:05 +0900

    회원가입,로그인 DB 연동

--- app/src/main/java/com/android/a13app/LoginActivity.kt
+++ app/src/main/java/com/android/a13app/LoginActivity.kt
@@ -1,12 +1,18 @@
 package com.android.a13app
 
 import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var dbManager: DBManager
+    lateinit var sqlitedb: SQLiteDatabase
+
     lateinit var edtId: EditText //아이디 입력값
     lateinit var edtPassword: EditText //비밀번호 입력값
     lateinit var btnLoginForm: Button //로그인 요청 버튼
@@ -18,9 +24,25 @@ class LoginActivity : AppCompatActivity() {
         edtPassword = findViewById(R.id.edtPassword)
         btnLoginForm = findViewById(R.id.btnLoginForm)
 
+        //DB 생성(friendshipDB)
+        dbManager = DBManager(this, "friendshipDB",null, 1)
+
         btnLoginForm.setOnClickListener {
-            var intent = Intent(this, ParentActivity::class.java)
-            startActivity(intent)
+            //사용자 입력값 가져오기
+            var str_id: String = edtId.text.toString()
+            var str_pw: String = edtPassword.text.toString()
+
+            //로그인 처리
+            sqlitedb = dbManager.readableDatabase
+
+            var cursor: Cursor
+            cursor = sqlitedb.rawQuery("SELECT * FROM tb_account WHERE id='"+str_id+"' AND pw='"+str_pw+"'", null)
+            if (cursor.count == 1) {
+                var intent = Intent(this, ParentActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "아이디와 비밀번호를 다시 확인해주세요", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/android/a13app/RegisterActivity.kt
+++ app/src/main/java/com/android/a13app/RegisterActivity.kt
@@ -1,12 +1,18 @@
 package com.android.a13app
 
 import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var dbManager: DBManager
+    lateinit var sqlitedb: SQLiteDatabase
+
     lateinit var edtId: EditText //아이디 입력값
     lateinit var edtPassword: EditText //비밀번호 입력값
     lateinit var edtName: EditText //사용자 이름 입력값
@@ -20,9 +26,37 @@ class RegisterActivity : AppCompatActivity() {
         edtName = findViewById(R.id.edtName)
         btnRegisterForm = findViewById(R.id.btnRegisterForm)
 
+        dbManager = DBManager(this, "friendshipDB", null, 1)
+
         btnRegisterForm.setOnClickListener {
-            var intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+            var str_id: String = edtId.text.toString()
+            var str_pw: String = edtPassword.text.toString()
+            var str_name: String = edtName.text.toString()
+
+            if (str_id == "") { //아이디 입력필수
+                Toast.makeText(this, "아이디를 입력하세요", Toast.LENGTH_SHORT).show()
+            } else if (str_pw == "") { //비밀번호 입력필수
+                Toast.makeText(this, "비밀번호를 입력하세요", Toast.LENGTH_SHORT).show()
+            } else if (str_name == "") { //이름 입력필수
+                Toast.makeText(this, "이름을 입력하세요", Toast.LENGTH_SHORT).show()
+            } else {
+                sqlitedb = dbManager.writableDatabase
+
+                //아이디 중복확인
+                var cursor: Cursor
+                cursor = sqlitedb.rawQuery("SELECT * FROM tb_account WHERE id='"+str_id+"'", null)
+                if (cursor.count > 0) {
+                    Toast.makeText(this, "이미 존재하는 아이디입니다", Toast.LENGTH_SHORT).show()
+                } else if (cursor.count == 0) { //중복되는 아이디가 없으면 회원정보 DB에 INSERT
+                    sqlitedb.execSQL("INSERT INTO tb_account VALUES ('"+str_id+"', '"+str_pw+"', '"+str_name+"')")
+                    sqlitedb.close()
+
+                    Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
+
+                    //로그인 페이지로 이동
+                    var intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)}
+            }
         }
     }
 }
