3992a46a7c59d2d58137e544cdce8a7b1fd09a68
GyeongHWa <hmgyng2017@gmail.com>
2024-01-28 23:43:47 +0900

    회원가입,로그인에서 Activity 전환

--- app/src/main/java/com/android/a13app/LoginActivity.kt
+++ app/src/main/java/com/android/a13app/LoginActivity.kt
@@ -1,11 +1,26 @@
 package com.android.a13app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var edtId: EditText //아이디 입력값
+    lateinit var edtPassword: EditText //비밀번호 입력값
+    lateinit var btnLoginForm: Button //로그인 요청 버튼
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        edtId = findViewById(R.id.edtId)
+        edtPassword = findViewById(R.id.edtPassword)
+        btnLoginForm = findViewById(R.id.btnLoginForm)
+
+        btnLoginForm.setOnClickListener {
+            var intent = Intent(this, ParentActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/android/a13app/RegisterActivity.kt
+++ app/src/main/java/com/android/a13app/RegisterActivity.kt
@@ -1,11 +1,28 @@
 package com.android.a13app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var edtId: EditText //아이디 입력값
+    lateinit var edtPassword: EditText //비밀번호 입력값
+    lateinit var edtName: EditText //사용자 이름 입력값
+    lateinit var btnRegisterForm: Button //회원가입 요청 버튼
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        edtId = findViewById(R.id.edtId)
+        edtPassword = findViewById(R.id.edtPassword)
+        edtName = findViewById(R.id.edtName)
+        btnRegisterForm = findViewById(R.id.btnRegisterForm)
+
+        btnRegisterForm.setOnClickListener {
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
