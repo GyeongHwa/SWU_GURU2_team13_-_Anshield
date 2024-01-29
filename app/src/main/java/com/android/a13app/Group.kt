60ae3c3ebb6a04878f92e928d4120d9f39d255d0
GyeongHWa <hmgyng2017@gmail.com>
2024-01-30 00:00:19 +0900

    홈 > 모임상세 버튼 구현

--- app/src/main/java/com/android/a13app/Group.kt
+++ app/src/main/java/com/android/a13app/Group.kt
@@ -1,8 +1,10 @@
 package com.android.a13app
 
-class Group(groupName: String, members: String) {
+class Group(groupName: String, members: String, token: String) {
     var groupName: String = groupName
         private set
     var members: String = members
+        private set
+    var token: String = token
         private set
 }
