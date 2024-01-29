f40434a08e110c15b48ab4a6aaed9826133be592
Hanbyulha <hjkl784519@gmail.com>
2024-01-29 18:49:35 +0900

    setFragment()추가 및 기타 코드 수정완료

--- app/src/main/java/com/android/a13app/DetailsFragment.kt
+++ app/src/main/java/com/android/a13app/DetailsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.children
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.a13app.databinding.FragmentDetailsBinding
 import java.util.Vector
@@ -12,9 +14,22 @@ import java.util.Vector
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class DetailsFragment : Fragment() {
+class DetailsFragment : Fragment(), View.OnClickListener {
     lateinit var binding: FragmentDetailsBinding
     lateinit var adapter: ExpenseCardAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setOnClickListener()
+    }
+    private fun setOnClickListener() {
+        val btnSequence = binding.layoutDatails.children
+        btnSequence.forEach { btn ->
+            btn.setOnClickListener(this)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,5 +54,18 @@ class DetailsFragment : Fragment() {
         binding!!.expenseRecyclerView.adapter = adapter
 
         return binding!!.root
+    }
+
+    override fun onClick(v: View) {
+        when (v.id){
+            R.id.btnAddExpense -> {
+                val parentActivity = activity as ParentActivity
+                parentActivity.setFragment(CalculateFragment())
+            }
+            R.id.btnCalculate ->{
+                val parentActivity = activity as ParentActivity
+                parentActivity.setFragment(ExpenseFragment())
+            }
+        }
     }
 }
