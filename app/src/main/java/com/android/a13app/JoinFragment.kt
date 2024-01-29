4ccb71142e01a71004509417535e622dcd0a532e
coderrim <hyorim2102@gmail.com>
2024-01-29 13:21:26 +0900

    모임참가... 근데 오류가 많음

--- app/src/main/java/com/android/a13app/JoinFragment.kt
+++ app/src/main/java/com/android/a13app/JoinFragment.kt
@@ -1,10 +1,16 @@
 package com.android.a13app
 
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 
 private const val ARG_PARAM1 = "param1"
@@ -14,11 +20,16 @@ class JoinFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    lateinit var joinEditText: EditText
+    lateinit var joinButton: Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+
         }
     }
 
