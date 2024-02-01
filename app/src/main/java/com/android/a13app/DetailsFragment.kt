0003425215a3aba7d68dc209461c5f9168299164
Hanbyulha <hjkl784519@gmail.com>
2024-02-01 13:57:07 +0900

    아이콘 변경

--- app/src/main/java/com/android/a13app/DetailsFragment.kt
+++ app/src/main/java/com/android/a13app/DetailsFragment.kt
@@ -1,8 +1,6 @@
 package com.android.a13app
 
 import android.app.ActionBar
-import android.database.Cursor
-import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,17 +9,15 @@ import android.view.ViewGroup
 import androidx.core.view.children
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.android.a13app.databinding.FragmentDetailsBinding
 import java.util.Vector
 
-class DetailsFragment : Fragment(), View.OnClickListener {
-    lateinit var dbManager: DBManager
-    lateinit var sqlitedb: SQLiteDatabase
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
+class DetailsFragment : Fragment(), View.OnClickListener {
     lateinit var binding: FragmentDetailsBinding
     lateinit var adapter: ExpenseCardAdapter
-    lateinit var memberAdapter: MemberAdapter
     lateinit var parentActivity: ParentActivity
     //버튼 클릭 이벤트 처리
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -44,19 +40,10 @@ class DetailsFragment : Fragment(), View.OnClickListener {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentDetailsBinding.inflate(inflater, container, false)
-
-        //모임정보 가져오기
-        val groupName: String = arguments?.getString("G_NAME").toString()
-        val token: String = arguments?.getString("TOKEN").toString()
-
         //title변경
         val actionBar = (activity as ParentActivity?)!!.supportActionBar
         actionBar!!.title = "모임이름"
-        actionBar.setDisplayHomeAsUpEnabled(true)
-
-        //DB연동
-        dbManager = DBManager(requireContext(), DBManager.DB_NAME,null, 1)
-        sqlitedb = dbManager.readableDatabase
+        actionBar.setDisplayHomeAsUpEnabled(false)
 
         //지출항목목록 리사이클러뷰
         val list = Vector<ExpenseCard>()
@@ -74,31 +61,6 @@ class DetailsFragment : Fragment(), View.OnClickListener {
 
         adapter = ExpenseCardAdapter(requireContext(), list)
         binding!!.expenseRecyclerView.adapter = adapter
-
-        //멤버 리사이클러뷰
-        val memberList = Vector<Member>()
-        val member: String = ""
-
-        var memberQuery = ""
-        memberQuery += "SELECT tb_account.name FROM tb_account "
-        memberQuery += "JOIN tb_member ON tb_account.id = tb_member.id "
-        memberQuery += "WHERE tb_member.token = '$token'"
-        var memberCursor: Cursor
-        memberCursor = sqlitedb.rawQuery(memberQuery, null)
-
-        while (memberCursor.moveToNext()) {
-            val memberItem = Member(
-                memberCursor.getString(memberCursor.getColumnIndexOrThrow("tb_account.name"))
-                    .toString()
-            )
-            memberList.add(memberItem)
-        }
-
-        val memberLayoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
-        binding!!.membersRecyclerView.layoutManager = memberLayoutManager
-
-        memberAdapter = MemberAdapter(requireContext(), memberList)
-        binding!!.membersRecyclerView.adapter = memberAdapter
 
         return binding!!.root
     }
